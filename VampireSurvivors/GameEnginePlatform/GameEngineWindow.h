1c57088e31b8b64cc36ca9274d8b9a4695bb3df9
이한성 <mast1207@naver.com>
2023-04-27 01:51:13 +0900

    업데이트

--- VampireSurvivors/GameEnginePlatform/GameEngineWindow.h
+++ VampireSurvivors/GameEnginePlatform/GameEngineWindow.h
@@ -1,1 +1,36 @@
 #pragma once
+#include <Windows.h>
+#include <string>
+
+// Ό³Έν :
+class GameEngineWindow
+{
+public:
+	static GameEngineWindow MainWindow;
+
+	// constrcuter destructer
+	GameEngineWindow();
+	~GameEngineWindow();
+
+	// delete Function
+	GameEngineWindow(const GameEngineWindow& _Other) = delete;
+	GameEngineWindow(GameEngineWindow&& _Other) noexcept = delete;
+	GameEngineWindow& operator=(const GameEngineWindow& _Other) = delete;
+	GameEngineWindow& operator=(GameEngineWindow&& _Other) noexcept = delete;
+
+	void Open(const std::string& _Title, HINSTANCE _hInstance);
+
+	static void MessageLoop(HINSTANCE _Inst, void(*_Start)(HINSTANCE), void(*_Update)(), void(*_End)());
+
+protected:
+
+private:
+	static HINSTANCE Instance;
+	std::string Title = "";
+	HWND hWnd = nullptr;
+
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	void InitInstance();
+	void MyRegisterClass();
+};
+
---
+++ VampireSurvivors/VampireSurvivorsApp/main.cpp
@@ -0,0 +1,28 @@
+#include <GameEnginePlatform/GameEngineWindow.h>
+#include <iostream>
+
+void TestStart(HINSTANCE _Inst)
+{
+    GameEngineWindow::MainWindow.Open("MainWindow", _Inst);
+}
+
+void TestEnd()
+{
+
+}
+
+void TestUpdate()
+{
+
+}
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    // CallBack 방식이라고 합니다.
+    GameEngineWindow::MessageLoop(hInstance, TestStart, TestUpdate, TestEnd);
+    return 0;
+}
